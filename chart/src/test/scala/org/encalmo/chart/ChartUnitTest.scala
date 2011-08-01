f46b1fec1f37ee683917844a5ccc3fe37a4de5db
opala.artur <opala.artur@localhost>
2011-08-01 08:00:50 +0200

    chart 30.07

--- chart/src/test/scala/org/encalmo/chart/ChartUnitTest.scala
+++ chart/src/test/scala/org/encalmo/chart/ChartUnitTest.scala
@@ -8,7 +8,6 @@ import java.io.IOException
 import java.util.logging.Level
 import java.util.logging.Logger
 import javax.imageio.ImageIO
-import org.jzy3d.chart.Chart
 import org.jzy3d.chart.controllers.keyboard.ChartKeyController
 import org.jzy3d.colors.Color
 import org.jzy3d.colors.ColorMapper
@@ -32,8 +31,10 @@ import org.jzy3d.plot3d.rendering.canvas.Quality
 import org.jzy3d.plot3d.rendering.legends.colorbars.ColorbarLegend
 import org.jzy3d.plot3d.rendering.view.modes.CameraMode
 import org.jzy3d.plot3d.rendering.view.modes.ViewBoundMode
-import org.jzy3d.plot3d.rendering.view.modes.ViewPositionMode;
+import org.jzy3d.plot3d.rendering.view.modes.ViewPositionMode
 import scalax.file.Path
+import org.jzy3d.plot3d.builder.concrete.SphereScatterGenerator
+import java.util.Random
 
 /** Tests chart generation with jyz3d */
 class ChartUnitTest {
@@ -70,7 +71,7 @@ class ChartUnitTest {
 		};
 		
 		// Create a chart 
-		val chart = new Chart(Quality.Advanced,"offscreen,1200,1200")
+		val chart = new org.jzy3d.chart.Chart(Quality.Advanced,"offscreen,1200,1200")
 		chart.getView().setAxeBoxDisplayed(true);
 		chart.getView().setCameraMode(CameraMode.ORTHOGONAL);
 		chart.getView().setBoundMode(ViewBoundMode.AUTO_FIT);
@@ -86,17 +87,102 @@ class ChartUnitTest {
 		//ColorbarLegend cbar = new ColorbarLegend(surface, chart.getView().getAxe().getLayout());
 		//surface.setLegend(cbar);
 		//chart.addController( new ChartKeyController());
-		createScreenshot(chart.getCanvas(), 0);
+		createScreenshot(chart.getCanvas(),"ColorWaveDemo", 0);
         chart.getView().setViewPoint(new Coord3d(java.lang.Math.PI/2,0,5));
-        createScreenshot(chart.getCanvas(), 1);
+        createScreenshot(chart.getCanvas(),"ColorWaveDemo", 1);
         chart.getView().setViewPoint(new Coord3d(0,0,5));
-        createScreenshot(chart.getCanvas(), 2);
+        createScreenshot(chart.getCanvas(),"ColorWaveDemo", 2);
         chart.getView().setViewPoint(new Coord3d(java.lang.Math.PI/4,java.lang.Math.PI/4,5));
-        createScreenshot(chart.getCanvas(), 3);
+        createScreenshot(chart.getCanvas(),"ColorWaveDemo", 3);
     }
     
-    def createScreenshot(ic:ICanvas, id:Int) {
-        val path = new File("target/screenshots",getClass().getSimpleName()+"_"+id+".png")
+    @Test def test2:Unit = {
+        
+        val coords = SphereScatterGenerator.generate(new Coord3d(10,20,30),10000,20, true)
+        val surface:Shape = Builder.buildDelaunay(coords).asInstanceOf[Shape]
+        val myColorMapper:ColorMapper = new ColorMapper(new ColorMapHotCold(), surface.getBounds().getZmin(), surface.getBounds().getZmax(), new Color(1,1,1,.5f))
+        surface.setColorMapper(myColorMapper)
+        surface.setFaceDisplayed(true)
+        surface.setWireframeDisplayed(false);
+        surface.setWireframeColor(Color.BLACK)
+        
+        // Create a chart 
+        val chart = new org.jzy3d.chart.Chart(Quality.Advanced,"offscreen,1200,1200")
+        chart.getView().setAxeBoxDisplayed(true);
+        chart.getView().setCameraMode(CameraMode.ORTHOGONAL);
+        chart.getView().setBoundMode(ViewBoundMode.AUTO_FIT);
+        chart.getView().setViewPositionMode(ViewPositionMode.FREE);
+        chart.getView().setViewPoint(new Coord3d(0,java.lang.Math.PI/2,5));
+
+        // Setup a colorbar for the surface object and add it to the scene
+        chart.getScene().getGraph().add(surface);
+        //ColorbarLegend cbar = new ColorbarLegend(surface, chart.getView().getAxe().getLayout());
+        //surface.setLegend(cbar);
+        //chart.addController( new ChartKeyController());
+        createScreenshot(chart.getCanvas(),"SphericDelaunaySurface", 0);
+        chart.getView().setViewPoint(new Coord3d(java.lang.Math.PI/2,0,5));
+        createScreenshot(chart.getCanvas(),"SphericDelaunaySurface", 1);
+        chart.getView().setViewPoint(new Coord3d(0,0,5));
+        createScreenshot(chart.getCanvas(),"SphericDelaunaySurface", 2);
+        chart.getView().setViewPoint(new Coord3d(java.lang.Math.PI/4,java.lang.Math.PI/4,5));
+        createScreenshot(chart.getCanvas(),"SphericDelaunaySurface", 3);
+    }
+    
+    @Test def test3:Unit = {
+        
+        import scala.collection.JavaConversions._
+        
+        val r = new Random();
+        
+        def coords:Seq[Coord3d] = for(x <- 0 to 1000) yield {
+            var x = 2.0*r.nextDouble();
+            var y = 2.0*r.nextDouble();
+            val s = r.nextDouble();            
+
+            if (s>0.75) {x = x * -1; y = y * -1}
+            if (s>0.5 && s<= 0.75) {x *= -1;}
+            if (s>0.25 && s<= 0.5) {y *= -1 ;}            
+
+            // exp( -(x**2 + y**2) ) * cos(x/4)*sin(y) * cos(2*(x**2+y**2))
+            val f1 = Math.cos(2*(x*x+y*y));
+            val f2 = Math.cos(x/4)*Math.sin(y);
+            val f3 = x*x + y*y;
+            val z = Math.pow(Math.E, -f3)*f2*f1;
+
+            new Coord3d(x,y,z)
+        }
+        
+        val surface:Shape = Builder.buildDelaunay(coords).asInstanceOf[Shape]
+        val myColorMapper:ColorMapper = new ColorMapper(new ColorMapRainbow(), surface.getBounds().getZmin(), surface.getBounds().getZmax(), new Color(1,1,1,.5f))
+        surface.setColorMapper(myColorMapper)
+        surface.setFaceDisplayed(true)
+        surface.setWireframeDisplayed(true);
+        surface.setWireframeColor(Color.BLACK)
+        
+        // Create a chart 
+        val chart = new org.jzy3d.chart.Chart(Quality.Advanced,"offscreen,1200,1200")
+        chart.getView().setAxeBoxDisplayed(true);
+        chart.getView().setCameraMode(CameraMode.ORTHOGONAL);
+        chart.getView().setBoundMode(ViewBoundMode.AUTO_FIT);
+        chart.getView().setViewPositionMode(ViewPositionMode.FREE);
+        chart.getView().setViewPoint(new Coord3d(0,java.lang.Math.PI/2,5));
+
+        // Setup a colorbar for the surface object and add it to the scene
+        chart.getScene().getGraph().add(surface);
+        //ColorbarLegend cbar = new ColorbarLegend(surface, chart.getView().getAxe().getLayout());
+        //surface.setLegend(cbar);
+        //chart.addController( new ChartKeyController());
+        createScreenshot(chart.getCanvas(),"GeneratedDelaunaySurface", 0);
+        chart.getView().setViewPoint(new Coord3d(java.lang.Math.PI/2,0,5));
+        createScreenshot(chart.getCanvas(),"GeneratedDelaunaySurface", 1);
+        chart.getView().setViewPoint(new Coord3d(0,0,5));
+        createScreenshot(chart.getCanvas(),"GeneratedDelaunaySurface", 2);
+        chart.getView().setViewPoint(new Coord3d(java.lang.Math.PI/4,java.lang.Math.PI/4,5));
+        createScreenshot(chart.getCanvas(),"GeneratedDelaunaySurface", 3);
+    }
+    
+    def createScreenshot(ic:ICanvas, name:String, id:Int) {
+        val path = new File("target/screenshots",name+"_"+id+".png")
         if (!path.exists()) {
             path.mkdirs();
         }
