42b1b71eef4f9d24e72c680bb28132ef7215b903
artur.opala <artur.opala@localhost>
2012-01-03 09:00:36 +0100

    units with context

--- calculation/src/main/scala/org/encalmo/calculation/MutableContext.scala
+++ calculation/src/main/scala/org/encalmo/calculation/MutableContext.scala
@@ -11,7 +11,14 @@ trait MutableContext extends Context {
   override def map:Map[Symbol,Expression]
   
   def update(s:Symbol, e:Expression) = {
-		if(!opened) throwException else map.put(symbol(s),e)
+		if(!opened) throwException else {
+		    val symb = symbol(s)
+		    val expr:Expression = e match {
+		        case v:Value => v.convertTo(s.unit)
+		        case _ => e
+		    }
+		    map.put(symb,expr)
+		}
   }
 
 }
