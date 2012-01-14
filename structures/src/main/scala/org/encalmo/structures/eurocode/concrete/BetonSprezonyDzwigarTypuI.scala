d4da296a7a7b8cea9b64ea12e7c0d3cefe161a42
artur.opala <artur.opala@localhost>
2012-01-14 09:38:38 +0100

    Merge with dev_20111228_units

--- structures/src/main/scala/org/encalmo/structures/eurocode/concrete/BetonSprezonyDzwigarTypuI.scala
+++ structures/src/main/scala/org/encalmo/structures/eurocode/concrete/BetonSprezonyDzwigarTypuI.scala
@@ -10,89 +10,69 @@ import org.encalmo.fop.FOPHelper
 import org.encalmo.structures.Predefined
 import org.encalmo.structures.Predefined._
 import org.encalmo.structures.eurocode.actions.ActionsSymbols
+import org.encalmo.structures.CalculationDocument
 
-class BetonSprezonyDzwigarTypuI {
+class BetonSprezonyDzwigarTypuI extends CalculationDocument {
     
+    override val name = "bks"
+        
     import BasicSymbols._
     import ConcreteSymbols._
-    import ActionsSymbols._
     
-    val daneMaterialowe,zestawienieObciazen,geometriaDzwigara,charakterystykiGeometryczne:Seq[Expression] = Seq()
-    
-    val calc = Calculation()
-    val beton = Concrete.C_50_60
+    val beton = Concrete.C_40_50
     calc add beton
     
-    val daneWejsciowe:Seq[Expression] = Seq(CLASS,fck,Ecm)
+    val gpk = g|"p,k" is "Charakterystyczny ciężar pokrycia dachu (płyt kanałowych i warstw izolacyjnych)" unit SI.kN/SI.m2; calc(gpk) = 4.0
+    val qsk = q|"s,k" is "Charakterystyczne obciążenie dachu śniegiem" unit SI.kN/SI.m2; calc(qsk) = 0.72
+    val qtk = q|"t,k" is "Charakterytyczne obciążenie technologiczne dźwigara" unit SI.kN/SI.m; calc(qtk) = 1.0
     
-    val doc1 = Document("",
+    val lr = l|r is "Rozstaw osi dźwigarów" unit SI.m; calc(lr) = 5.5
+    val ld = l|d is "Długość dźwigara w osi podpór" unit SI.m; calc(ld) = 14
+    
+    val hd = h|d is "Wysokość przekroju dźwigara" unit SI.cm acc 5; calc(hd) = ld/20
+	val bd = b|d is "Szerokość przekroju dźwigara" unit SI.cm acc 1; calc(bd) = hd/8
+	val b1 = b|1 is "Szerokość środnika dźwigara" unit SI.cm acc 1; calc(b1) = bd/3
+	val b2 = b|2 is "Szerokość półki dźwigara" unit SI.cm; calc(b2) = (bd-b1)/2
+    val h1 = h|1 is "Wysokość półki górnej przekroju" unit SI.cm; calc(h1) = 10
+    val h2 = h|2 is "Wysokość skosu półki górnej przekroju" unit SI.cm acc 1; calc(h2) = b2/2
+	val h4 = h|4 is "Wysokość skosu półki dolnej przekroju" unit SI.cm acc 1; calc(h4) = b2/1.5
+	val h5 = h|5 is "Wysokość półki dolnej przekroju" unit SI.cm; calc(h5) = 20
+	val h3 = h|3 is "Wysokość środnika przekroju" unit SI.cm; calc(h3) = hd-h1-h2-h4-h5
+
+    val Ac = A|c is "Pole przekroju betonowego" unit SI.cm2
+	calc(Ac) = h1*bd+h5*bd+(h2+h3+h4)*b1+h2*b2+h4*b2
+	val gwk = g|"c,k" is "Charakterystyczny ciężar własny dźwigara" unit SI.kN/SI.m
+	calc(gwk) = Ac*gammac
+	
+    val qdmax = q|"d,max" is "Sumaryczne obliczeniowe obciążenie dźwigara (maksymalne)" unit SI.kN/SI.m acc 0.1; calc(qdmax) = gpk*lr*1.35 + qsk*lr*1.5 + qtk*1.5
+    
+    
+    val doc = Document("",
         Predefined.stylesConfig,
         Chapter("",
         	Section(
-            	Section("Ćwiczenie projektowe z przedmiotu \"Betonowe Konstrukcje Sprężone\". Semestr zimowy 2010/2011."),
+            	Section("Ćwiczenie projektowe z przedmiotu \"Betonowe Konstrukcje Sprężone\". Semestr zimowy 2011/2012."),
         		Section("Autor: Artur Opala, album nr 61315. Prowadzący: dr inż. Aleksy Łodo, Instytut Budownictwa Politechiki Wrocławskiej.")
         	),
             Section(""),
             NumSection("Zadanie",
                 Section(styleComment,"Dźwigar dachowy z betonu sprężonego."),
                 NumSection("Normy i literatura",
-                	Section(styleComment," [1] Norma PN-EN 1995-1-1:2010 \"Eurokod 5. Projektowanie konstrukcji drewnianych. Część 1-1: Postanowienia ogólne. Reguły ogólne i reguły dotyczące budynków\""),
-                	Section(styleComment," [2] Aprobata ITB AT-15-7343/2007 \"Wkręty samowiercące SPAX&reg; do konstrukcji drewnianych\""),
-                	Section(styleComment," [3] Norma PN-EN 338:2009")
+                	Section(styleComment," [1] Norma PN-EN 1992-1-1 \"Eurokod 2. Projektowanie konstrukcji z betonu. Część 1-1: Reguły ogólne i reguły dla budynków.\"")
+                	
                 ),
-                NumSection("Dane wejściowe",Evaluate(daneWejsciowe,calc))
+                NumSection("Parametry zadania",Evaluate(calc,ld,lr,beton(CLASS),gpk,qsk,qtk)),
+                beton.info,
+                NumSection("Geometria przekroju dźwigara",Evaluate(calc,hd,bd,b1,b2,h1,h2,h4,h5,h3,Ac,gwk)),
+                NumSection("Wyznaczenie liczby cięgien sprężających",Evaluate(calc,qdmax)),
+                NumSection("Wyznaczenie charakterystyk przekroju dźwigara",Evaluate(calc))
            ),
-           /*NumSection("Dane do obliczeń",
-                NumSection("Przyjęte wymiary przekroju słupa",Evaluate(calc,przyjetaGeometria:_*)),
-                NumSection("Przyjęte łączniki",Evaluate(calc,przyjeteLaczniki:_*)),
-                NumSection("Właściwości geometryczne przekroju",Evaluate(calc,wlasciwosciGeometryczne:_*)),
-                NumSection("Współczynniki",Evaluate(calc,wspolczynnikiCzesciowe:_*)),
-                NumSection("Właściwości mechaniczne charakterystyczne dla drewna litego klasy C27 wg [3]",Evaluate(calc,wlasciwosciMechaniczneCharakterystyczne:_*)),
-                NumSection("Właściwości mechaniczne obliczeniowe dla 2 klasy użytkowania i obciążeń długotrwałych",Evaluate(calc,wlasciwosciMechaniczneObliczeniowe:_*))),
-           NumSection("Sprawdzenie stanów granicznych nośności wg PN-EN 1995-1-1",
-                NumSection("Obliczenie sztywności zastępczej",Evaluate(calc,sztywnoscZastepcza:_*)),
-                NumSection("Sprawdzenie nośności na ściskanie",Evaluate(calc,nosnoscObliczeniowa:_*)),
-                Section(styleWarunek,"Warunek C.1 [1] jest spełniony: ",
-                                Symb(sigmac0d),LE,Symb(kc),Symb(fc0d),ARROW,Result(calc,sigmac0d),LE,Result(calc,kc*fc0d)),
-				NumSection("Sprawdzenie nośności środnika na ścinanie",Evaluate(calc,nosnoscScinanie:_*)),
-				Section(styleWarunek,"Warunek 6.13 [1] jest spełniony: ",
-                                Symb(tau2max),LE,Symb(fvd),ARROW,Result(calc,tau2max),LE,Result(calc,fvd)),
-				NumSection("Sprawdzenie nośności łączników (wkrętów)",Evaluate(calc,nosnoscLacznikow:_*)),
-				Section(styleWarunek,"Warunek nośności wkręta jest spełniony: ",
-                                Symb(Fi),LE,Symb(FvRd),ARROW,Result(calc,Fi),LE,Result(calc,FvRd))
-			),
-			NumSection("Badania porównawcze",
-				NumSection("Porównanie ze słupem wielogałęziowym z elementów połączonych niepodatnie (klejonych)",Evaluate(calc,nosnoscSlupaNiepodatnie:_*)),
-				NumSection("Porównanie ze słupem jednorodnym kwadratowym o zbliżonej nośności na ściskanie",Evaluate(calc4,przekrojKwadratowy:_*)),
-				NumSection("Wnioski",
-					Section(styleComment,"Porównanie zaprojektowanego słupa ze słupem klejonym wykazało ",Result(calc,round(deltaJW,RoundingMode.HALF)),
-					"% utratę nośności ze względu na podatność łączników."),
-					Section(styleComment,"Porównanie zaprojektowanego słupa ze słupem jednorodnym kwadratowym o zbliżonej nośności wykazało ",Result(calc4,round(delta2,RoundingMode.HALF)),
-					"% stratę na przekroju słupa wielogałęziowego. Zastosowanie słupa o przekroju kwadratowym przy zadanym obciążeniu byłoby bardziej uzasadnione.")
-				)
-			),*/
+           
 			Section(style1.marginTop(20),""),
 			Section("Koniec obliczeń."),
 			Section(style1.marginTop(20),""),
 			Section(style1.useAlign("right"),"Opracował: Artur Opala")
         )
     )
-    
-    @Test def printPdf:Unit = {
-        val layout = Predefined.layout
-        val output:XslFoOutput = new XslFoOutput(layout, new java.util.Locale("PL"))
-        output.open
-        XslFoTextDocumentPrinter.print(doc1,output)
-        output.close
-        output.printConsole
-        output.saveToFile(new java.io.File("target/test-results/ks-dzwigar.fo"))
-        FOPHelper.buildPDF(output.getResult, "target/test-results/ks-dzwigar.pdf")
-    }
-    
-    @Test def printText:Unit = {
-        val o:TextOutput = new TextOutput(new java.util.Locale("PL"))
-        PlainTextDocumentPrinter.print(doc1,o)
-        o.printConsole
-    }
 
 }
