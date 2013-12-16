dcbbdfef8e3daa4607c82063f8d0819149806b1d
opala artur <opala.artur@gmail.com>
2013-12-17 00:08:09 +0100

    form for mutli storey carpark

--- webapp/app/controllers/Application.scala
+++ webapp/app/controllers/Application.scala
@@ -6,6 +6,7 @@ import scala.concurrent.Future
 import org.encalmo.structures.miscellaneous.AOMTWorksheet
 import org.encalmo.printer.{HtmlOutputPreferences, HtmlOutput}
 import org.encalmo.style.PredefinedStyles
+import forms.MultiStoreyCarParkInput
 
 object Application extends Controller {
 
@@ -15,17 +16,31 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
   
-  def multistoreycarpark = Action.async {
-    val f = Future[String] {
-      val layout = PredefinedStyles.layout
-      val prefs: HtmlOutputPreferences = HtmlOutputPreferences().asFragmentOfPage().ignoreDocumentStyles(true)
-      val output: HtmlOutput = new HtmlOutput(layout, new java.util.Locale("PL"), prefs)
-      new AOMTWorksheet().renderHtml(output)
-      output.getResult
-    }
-    f map {
-      case result =>  Ok(views.html.multistoreycarpark(result))
-    }
+  def multistoreycarpark = Action.async { implicit request =>
+    val form = MultiStoreyCarParkInput.form.bindFromRequest()
+    form.fold (
+      formWithErrors => {
+        // binding failure, you retrieve the form containing errors:
+        Future{
+          BadRequest(views.html.multistoreycarpark("",formWithErrors))
+        }
+      },
+      userData => {
+        /* binding success, you get the actual value. */
+        val f = Future[String] {
+          val layout = PredefinedStyles.layout
+          val prefs: HtmlOutputPreferences = HtmlOutputPreferences().asFragmentOfPage().ignoreDocumentStyles(true)
+          val output: HtmlOutput = new HtmlOutput(layout, new java.util.Locale("PL"), prefs)
+          new AOMTWorksheet().renderHtml(output)
+          output.getResult
+        }
+        f map {
+          case result =>  Ok(views.html.multistoreycarpark(result,form))
+        }
+
+      }
+    )
+
   }
 
 }
